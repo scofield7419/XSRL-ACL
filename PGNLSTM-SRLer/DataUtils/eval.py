6c63d03f8b33ac26e89cb35a0c646e026dc2220e
Scofield Phil <feish7419@163.com>
2020-07-12 07:56:37 +0800

    Update eval.py

--- PGNLSTM-SRLer/DataUtils/eval.py
+++ PGNLSTM-SRLer/DataUtils/eval.py
@@ -1,4 +1,3 @@
-
 class Eval:
     def __init__(self):
         self.predict_num = 0
@@ -28,7 +27,10 @@ class Eval:
             true_labels = [item for item in y_true[i][:sentence_length] if item != '_']
             pred_labels = [item for item in y_pred[i][:sentence_length] if item != '_']
 
-            self.correct_num += len(set(true_labels) & set(pred_labels))
+            self.correct_num = 0
+            for p_lable, g_lable in zip(pred_labels, true_labels):
+                if p_lable == g_lable:
+                    self.correct_num += += 1
             self.predict_num += len(set(pred_labels))
             self.gold_num += len(set(true_labels))
 
@@ -51,7 +53,7 @@ class Eval:
 
         return self.precision, self.recall, self.fscore, self.acc
 
-    def accuracy(self, predict_labels, gold_labels, all_sentence_length):  # wrong
+    def accuracy(self, predict_labels, gold_labels, all_sentence_length): 
         cor = 0
         totol_leng = sum([len(predict_label) for predict_label in predict_labels])
 
@@ -63,65 +65,3 @@ class Eval:
         self.acc = cor / totol_leng * 100
 
         return self.acc
-
-
-class EvalPRF:
-    def evalPRF(self, predict_labels, gold_labels, eval):
-        gold_ent = self.get_ent(gold_labels)
-        predict_ent = self.get_ent(predict_labels)
-        eval.predict_num += len(predict_ent)
-        eval.gold_num += len(gold_ent)
-
-        count = 0
-        for p in predict_ent:
-            if p in gold_ent:
-                count += 1
-                eval.correct_num += 1
-
-    def get_ent(self, labels):
-        idx = 0
-        idy = 0
-        endpos = -1
-        ent = []
-        while (idx < len(labels)):
-            if (self.is_start_label(labels[idx])):
-                idy = idx
-                endpos = -1
-                while (idy < len(labels)):
-                    if not self.is_continue_label(labels[idy], labels[idx], idy - idx):
-                        endpos = idy - 1
-                        break
-                    endpos = idy
-                    idy += 1
-                ent.append(self.cleanLabel(labels[idx]) + '[' + str(idx) + ',' + str(endpos) + ']')
-                idx = endpos
-            idx += 1
-        return ent
-
-    def cleanLabel(self, label):
-        start_label = ['B', 'b', 'M', 'm', 'E', 'e', 'S', 's', 'I', 'i']
-        if len(label) > 2 and label[1] == '-':
-            if label[0] in start_label:
-                return label[2:]
-        return label
-
-    def is_continue_label(self, label, startLabel, distance):
-        if distance == 0:
-            return True
-        if len(label) < 3:
-            return False
-        if distance != 0 and self.is_start_label(label):
-            return False
-        if (startLabel[0] == 's' or startLabel[0] == 'S') and startLabel[1] == '-':
-            return False
-        if self.cleanLabel(label) != self.cleanLabel(startLabel):
-            return False
-        return True
-
-    def is_start_label(self, label):
-        start = ['b', 'B', 's', 'S']
-        # start = ['b', 'B']
-        if len(label) < 3:
-            return False
-        else:
-            return (label[0] in start) and label[1] == '-'
